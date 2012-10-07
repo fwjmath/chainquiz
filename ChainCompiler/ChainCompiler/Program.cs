fd0e12980fc45a4b6479d810409c5e6a8ea92397
fwjmath@gmail.com <fwjmath@gmail.com@109f10dd-a77d-5f23-d608-26eac85a1126>
2012-10-07 19:25:58 +0000

    initial dispose

---
+++ ChainCompiler/ChainCompiler/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ChainCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename;
+            if (args.Length == 0)
+            {
+                filename = "test.txt";
+            }
+            else
+            {
+                filename = args[0];
+            }
+
+            StreamReader sr = new StreamReader(filename, Encoding.UTF8);
+            StreamWriter sw = new StreamWriter(filename + ".qz", false, Encoding.UTF8);
+
+            string quizName = sr.ReadLine();
+            string quizDescription = sr.ReadLine();
+            int quizLenInput = Convert.ToInt32(sr.ReadLine());
+            string verifSalt = sr.ReadLine();
+            string keySalt = sr.ReadLine();
+            string endMessage = sr.ReadLine();
+            AesCryptoServiceProvider AESMachine = new AesCryptoServiceProvider();
+            SHA256 SHAMachine = SHA256Managed.Create();
+            UTF8Encoding encoding = new UTF8Encoding();
+
+            sw.WriteLine(quizName);
+            sw.WriteLine(quizDescription);
+            sw.WriteLine(Convert.ToString(quizLenInput));
+            sw.WriteLine(verifSalt);
+            sw.WriteLine(keySalt);
+
+            AESMachine.Padding = PaddingMode.ISO10126;
+            AESMachine.Mode = CipherMode.CBC;
+            AESMachine.KeySize = 256;
+            SHAMachine.Initialize();
+
+            byte[] key = new byte[256];
+            key = SHAMachine.ComputeHash(encoding.GetBytes(quizName + keySalt));
+
+            int quizLen = 0;
+
+            while (!sr.EndOfStream)
+            {
+                string quizQuestion = "", quizAnswer, currentLine;
+                
+                do
+                {
+                    currentLine = sr.ReadLine();
+                } while (currentLine != ">>>");
+
+                currentLine = sr.ReadLine();
+
+                while (currentLine != "<<<")
+                {
+                    quizQuestion += currentLine + "\n";
+                    currentLine = sr.ReadLine();
+                }
+
+                quizAnswer = sr.ReadLine();
+
+                // Process here
+
+                sw.WriteLine(Convert.ToBase64String(SHAMachine.ComputeHash(encoding.GetBytes(verifSalt + quizAnswer))));
+
+                AESMachine.GenerateIV();
+                AESMachine.Key = key;
+                ICryptoTransform AESEncrypt = AESMachine.CreateEncryptor();
+                sw.WriteLine(Convert.ToBase64String(AESMachine.IV));
+
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, AESEncrypt, CryptoStreamMode.Write);
+                cs.Write(encoding.GetBytes(quizQuestion),0,encoding.GetByteCount(quizQuestion));
+                cs.FlushFinalBlock();
+                sw.WriteLine(Convert.ToBase64String(ms.ToArray()));
+
+                key = SHAMachine.ComputeHash(encoding.GetBytes(quizAnswer + keySalt));
+                quizLen++;
+            }
+
+            if (quizLen != quizLenInput)
+            {
+                Console.WriteLine("Error!");
+                Console.ReadKey();
+            }
+            else
+            {
+                AESMachine.GenerateIV();
+                AESMachine.Key = key;
+                ICryptoTransform AESEncrypt = AESMachine.CreateEncryptor();
+                sw.WriteLine(Convert.ToBase64String(AESMachine.IV));
+
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, AESEncrypt, CryptoStreamMode.Write);
+                cs.Write(encoding.GetBytes(endMessage), 0, encoding.GetByteCount(endMessage));
+                cs.FlushFinalBlock();
+                sw.WriteLine(Convert.ToBase64String(ms.ToArray()));
+            }
+
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
---
+++ ChainQuiz/ChainQuiz/Form2.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ChainQuiz
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxQuestions = new System.Windows.Forms.ListBox();
+            this.textBoxAns = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.textBoxQue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBoxQuestions
+            // 
+            this.listBoxQuestions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxQuestions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxQuestions.FormattingEnabled = true;
+            this.listBoxQuestions.ItemHeight = 16;
+            this.listBoxQuestions.Location = new System.Drawing.Point(8, 8);
+            this.listBoxQuestions.Name = "listBoxQuestions";
+            this.listBoxQuestions.Size = new System.Drawing.Size(137, 548);
+            this.listBoxQuestions.TabIndex = 0;
+            this.listBoxQuestions.SelectedIndexChanged += new System.EventHandler(this.listBoxQuestions_SelectedIndexChanged);
+            // 
+            // textBoxAns
+            // 
+            this.textBoxAns.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAns.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAns.Location = new System.Drawing.Point(151, 531);
+            this.textBoxAns.Name = "textBoxAns";
+            this.textBoxAns.Size = new System.Drawing.Size(677, 22);
+            this.textBoxAns.TabIndex = 1;
+            this.textBoxAns.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxAns_KeyPress);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCheck.Location = new System.Drawing.Point(834, 528);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(71, 26);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // textBoxQue
+            // 
+            this.textBoxQue.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxQue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxQue.Location = new System.Drawing.Point(155, 11);
+            this.textBoxQue.Multiline = true;
+            this.textBoxQue.Name = "textBoxQue";
+            this.textBoxQue.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxQue.Size = new System.Drawing.Size(749, 511);
+            this.textBoxQue.TabIndex = 3;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 562);
+            this.Controls.Add(this.textBoxQue);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.textBoxAns);
+            this.Controls.Add(this.listBoxQuestions);
+            this.Name = "Form2";
+            this.Text = "ChainQuiz - Quiz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxQuestions;
+        private System.Windows.Forms.TextBox textBoxAns;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox textBoxQue;
+    }
+}
